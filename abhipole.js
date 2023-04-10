b672f2ffff19f0db14ff79c83c41eeb8d60e861b
Abhishek Pandey <abhishek.pandey@ucertify.com>
2023-04-10 13:23:06 +0530

    dv

--- abhipole.js
+++ abhipole.js
@@ -5,4 +5,4 @@ const num2 = 3;
 const sum = num1 + num2;
 
 // display the sum
-console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+console.log(`The sum of ${num1} and ${num2} is: ${sum}`);
